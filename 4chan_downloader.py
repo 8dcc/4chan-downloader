834475a9bc8368f2d987ec43ac9c49dda2cad508
ravi0li <mikel.veg777@gmail.com>
2021-08-31 07:43:38 +0200

    v1.5
    Added an option for storing the cookies

--- 4chan_downloader.py
+++ 4chan_downloader.py
@@ -13,6 +13,7 @@ except Exception:
 ############ EDIT ME ############
 useTorProxy = False             #  << Put here True or False
 debugPrint = False              #  << Put here True or False
+sessionMode = False             #  << Put here True or False
 #################################
 
 def check_variable_types():
@@ -34,12 +35,17 @@ def main():
 	check_variable_types()
 	banner()
 
+	s = requests.Session() # Create a session because of the cookie
+
 	if useTorProxy == False:
 		proxies = ""
 	elif useTorProxy == True:
 		proxies = {'http': 'socks5://127.0.0.1:9150', 'https': 'socks5://127.0.0.1:9150'}
 		try:
-		    requests.get("https://4chan.org/", proxies=proxies)
+			if sessionMode:
+				s.get("https://4chan.org/", proxies=proxies)
+			else:
+				requests.get("https://4chan.org/", proxies=proxies)
 		except Exception:
 		    exit(f" {Style.RESET_ALL}{Fore.RED}[!] We could not verify the proxies. Make sure tor is running.{Style.RESET_ALL}")
 
@@ -61,7 +67,10 @@ def main():
 			URL = "https://boards.4channel.org/" + board + "/" + str(pageNumber)
 			if pageNumber == 1:
 				URL = "https://boards.4channel.org/" + board + "/"
-			r = requests.get(URL, proxies=proxies, allow_redirects=True)
+			if sessionMode:
+				r = s.get(URL, proxies=proxies, allow_redirects=True)
+			else:
+				r = requests.get(URL, proxies=proxies, allow_redirects=True)
 			if "Attention Required! | Cloudflare" in r.text:
 				exit(f" {Style.RESET_ALL}{Fore.RED}[!] Cloudflare captcha needed. Exiting...{Style.RESET_ALL}")
 			souped = BeautifulSoup(r.text, 'html.parser')
@@ -69,15 +78,29 @@ def main():
 			for img in img_tags:
 				img_scr = img.get('src')
 				if debugPrint:
-					print(img_scr)
+					print(f"IMG_SCR: {img_scr}\n")
 				if f"i.4cdn.org/{board}" in img_scr:
 					if "http" not in img_scr:
 						img_scr = f"https:{img_scr}"
 						img_id = img_scr.split("/")[-1].split(".")[0].replace("s", "")
-						sys.stdout.write(f"\r {Style.RESET_ALL}{Style.BRIGHT}[{Fore.GREEN}+{Style.RESET_ALL}{Style.BRIGHT}] Downloading {Style.RESET_ALL}{Fore.GREEN}" + img_id + f"{Style.RESET_ALL}")
+						sys.stdout.write(f"\r {Style.RESET_ALL}{Style.BRIGHT}[{Fore.GREEN}+{Style.RESET_ALL}{Style.BRIGHT}] Downloading {Style.RESET_ALL}{Fore.GREEN}{img_id}{Style.RESET_ALL}")
 						sys.stdout.flush()
 						img_url = "https://i.4cdn.org/" + board + "/" + img_id + ".jpg"
-						r2 = requests.get(img_url)
+						if debugPrint:
+							print(f"IMG_URL: {img_url}\n")
+						if sessionMode:
+							r2 = s.get(img_url)
+						else:
+							r2 = requests.get(img_url)
+						# print(r2.text)  # Inspect every request for debug
+						if "Checking your browser" in r2.text:
+							sys.stdout.write(f"\r {Style.RESET_ALL}{Style.BRIGHT}[{Fore.RED}!{Style.RESET_ALL}{Style.BRIGHT}] 4chan is requesting a human verification for image: {Fore.RED}{img_id}{Style.RESET_ALL}")
+							sys.stdout.flush()
+							print()
+						elif "404 Not Found" in r2.text:
+							sys.stdout.write(f"\r {Style.RESET_ALL}{Style.BRIGHT}[{Fore.RED}!{Style.RESET_ALL}{Style.BRIGHT}] 4chan returned 404 for image: {Fore.RED}{img_id}{Style.RESET_ALL}")
+							sys.stdout.flush()
+							print()
 						if r2.headers["content-type"] in images:
 							with open("4chan_downloads/" + img_id + ".jpg", "wb") as i:
 								i.write(r2.content)
